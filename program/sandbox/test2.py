4f304931446dd423d881872bf1c3a7dab55a9dd4
S0mirun <shintototo1019@icloud.com>
2025-10-23 10:29:37 +0900

    a

--- program/sandbox/test2.py
+++ program/sandbox/test2.py
@@ -1,6 +1,3 @@
-
-U = [0,0,0,1,1,0,1,0,0]
-stay = 0
-for u in U:
-    stay = stay + 1 if u == 0 else 0
-    print(u, stay)
+import pandas as pd
+df = pd.read_pickle("df_rest_all.pickle")  # 圧縮付きでもOK
+print(type(df), getattr(df, "shape", None))
