0ad4fb8916f6ef15f9e3d7cca63bcc9c7ae27072
S0mirun <shintototo1019@icloud.com>
2026-04-24 15:57:40 +0900

    分割しながら見てるけどいい港ない

--- program/path_planning/Visualize_data.py
+++ program/path_planning/Visualize_data.py
@@ -4,8 +4,10 @@ import re
 import glob
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import geopandas as gpd
 
+from scipy.spatial import cKDTree
 from shapely.geometry import LineString
 from shapely.ops import unary_union, polygonize, polygonize_full, snap
 from tqdm import tqdm
@@ -14,6 +16,7 @@ JAPAN = True
 BUOY = True
 AIS = True
 
+GRID = True
 ZOOM = False
 
 
@@ -46,7 +49,6 @@ def draw_Japan_poly(fig, ax):
     _save_fig(fig, ax, "Japan")
     
 
-
 def _make_lines():
     csv_path = glob.glob("raw_datas/国土交通省/*/*.csv")
 
@@ -75,27 +77,90 @@ def _make_lines():
 def _make_land_polygon(lines):
     print("\nmerging lines...")
     merged = unary_union(lines)
-    check_polygon(merged)
+    check_polygon(merged, fig, ax)
 
     print("\nsnapping...")
-    snapped = snap(merged, merged, 1e-4)
-    check_polygon(snapped)
+    # snapped = _snap_lines(lines)
+    # snapped = snap(merged, merged, 1e-4)
+    # check_polygon(snapped)
 
     print("\npolygonizing...")
-    # polygons = list(polygonize(merged))
-    polygons = list(polygonize(snapped))
+    polygons = list(polygonize(merged))
+    # polygons = list(polygonize(snapped))
 
     print(f"\npolygons: {len(polygons)}")
     return polygons
 
 
-def check_polygon(polygon):
+def check_polygon(polygon, fig, ax):
     polys, cuts, dangles, invalids = polygonize_full(polygon)
+
+    gdf_poly = gpd.GeoDataFrame(geometry=list(polys.geoms), crs="EPSG:4326")
+    gdf_dng = gpd.GeoDataFrame(geometry=list(dangles.geoms), crs="EPSG:4326")
+
+    gdf_poly.plot(ax=ax, color="lightgray", edgecolor="none")
+    gdf_dng.plot(ax=ax, color="red", linewidth=1.0)
 
     print("polygons :", len(list(polys.geoms)))
     print("cuts     :", len(list(cuts.geoms)))
     print("dangles  :", len(list(dangles.geoms)))
     print("invalids :", len(list(invalids.geoms)))
+
+
+def _snap_lines(lines):
+    endpoint_records = []
+    endpoint_xy = []
+    for i, line in enumerate(lines):
+        coords = list(line.coords)
+
+        p0 = coords[0]
+        p1 = coords[-1]
+
+        endpoint_records.append((i, 0))
+        endpoint_xy.append(p0)
+
+        endpoint_records.append((i, -1))
+        endpoint_xy.append(p1)
+
+    endpoint_xy = np.array(endpoint_xy, dtype=float)
+
+    tree = cKDTree(endpoint_xy)
+    pairs = tree.query_pairs(r=1e-8)
+
+    replacement = {}
+    for a, b in pairs:
+        xa, ya = endpoint_xy[a]
+        xb, yb = endpoint_xy[b]
+
+        xm = (xa + xb) / 2
+        ym = (ya + yb) / 2
+
+        replacement[a] = (xm, ym)
+        replacement[b] = (xm, ym)
+
+    snapped = []
+    for i, line in enumerate(lines):
+        coords = list(line.coords)
+        if len(coords) < 2:
+            snapped.append(line)
+            continue
+
+        # この line の始点・終点に対応する endpoint index を探す
+        start_idx = 2 * i
+        end_idx = 2 * i + 1
+
+        if start_idx in replacement:
+            coords[0] = replacement[start_idx]
+
+        if end_idx in replacement:
+            coords[-1] = replacement[end_idx]
+
+        try:
+            snapped.append(LineString(coords))
+        except Exception:
+            snapped.append(line)
+
+    return snapped
 
 
 def _draw_polygons(fig, ax, polygons):
@@ -186,8 +251,60 @@ def draw_AIS(fig, ax):
     _save_fig(fig, ax, "AIS")
 
 
+def save_grid_fig(fig, ax, xmin=127.5, xmax=146.5, ymin=27.0, ymax=46.0, step=2.5):
+    xs = np.arange(xmin, xmax, step)
+    ys = np.arange(ymin, ymax, step)
+
+    save_dir = f"outputs/{dirname}/tile"
+    os.makedirs(save_dir, exist_ok=True)
+    count = 0
+    for y0 in tqdm(ys):
+        for x0 in xs:
+            x1 = x0 + step
+            y1 = y0 + step
+
+            ax.set_xlim(x0, x1)
+            ax.set_ylim(y0, y1)
+            ax.set_aspect("equal", adjustable="box")
+
+            filename = f"tile_lat_{y0:.2f}_{y1:.2f}_lon_{x0:.2f}_{x1:.2f}.png"
+            fig.savefig(
+                os.path.join(save_dir, filename),
+                dpi=400,
+                bbox_inches="tight",
+                pad_inches=0.05,
+            )
+            count += 1
+
+    print("\ntiles saved")
+
+
 def save_zoom_fig(fig, ax):
-    pass
+    port_path = glob.glob("raw_datas/tmp/coordinates_of_port/*.csv")
+    margin = 0.02
+
+    for path in port_path:
+        df = pd.read_csv(path)
+
+        if "Longitude" not in df.columns:
+            continue
+        if "Latitude" not in df.columns:
+            continue
+
+        lon = df["Longitude"].iloc[0]
+        lat = df["Latitude"].iloc[0]
+
+        ax.set_xlim(lon-margin, lon+margin)
+        ax.set_ylim(lat-margin, lat+margin)
+        
+
+        port_name = os.path.splitext(os.path.basename(path))[0]
+        save_dir = f"outputs/{dirname}/port"
+        os.makedirs(save_dir, exist_ok=True)
+
+        ax.set_aspect("equal", adjustable="box")
+        fig.savefig(os.path.join(save_dir, f"{port_name}.png"), 
+                    dpi=400, bbox_inches="tight", pad_inches=0.05)
 
 
 if __name__ == '__main__':
@@ -201,6 +318,9 @@ if __name__ == '__main__':
 
     if AIS:
         draw_AIS(fig, ax)
+
+    if GRID:
+        save_grid_fig(fig, ax)
 
     if ZOOM:
         save_zoom_fig(fig, ax)
